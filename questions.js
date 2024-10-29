558f6e8b740b8a7e7fcd40ca82d7b8f78e84e537
SahFlo <sahal.florence@gmail.com>
2024-10-29 15:53:55 +0100

    Questions.js

---
+++ questions.js
@@ -0,0 +1,86 @@
+export const quizTableau = {
+    questions: [
+      {
+        text: "Quelle est la capitale de la France",
+        options: [
+          "Marseille",
+          "Lyon",
+          "Bordeaux",
+          "Paris"
+        ],
+        correct_answer: "Paris"
+      },
+      {
+        text: "Quelle est la capitale du Canada",
+        options: [
+          "Toronto",
+          "Ottawa",
+          "Vancouver",
+          "Montréal"
+        ],
+        correct_answer: "Ottawa"
+      },
+      {
+        text: "Quelle est la capitale du Japon",
+        options: [
+          "Tokyo",
+          "Kyoto",
+          "Osaka",
+          "Yokohama"
+        ],
+        correct_answer: "Tokyo"
+      },
+      {
+        text: "Quelle est la capitale de l'Australie",
+        options: [
+          "Sydney",
+          "Canberra",
+          "Melbourne",
+          "Perth"
+        ],
+        correct_answer: "Canberra"
+      },
+      {
+        text: "Quelle est la capitale de la Suisse",
+        options: [
+          "Zurich",
+          "Genève",
+          "Lausanne",
+          "Berne"
+        ],
+        correct_answer: "Berne"
+      },
+      {
+        text: "Quelle est la capitale de la Nouvelle-Zélande",
+        options: [
+          "Auckland",
+          "Wellington",
+          "Christchurch",
+          "Hamilton"
+        ],
+        correct_answer: "Wellington"
+      },
+      {
+        text: "Quelle est la capitale de la Lituanie",
+        options: [
+          "Riga",
+          "Tallinn",
+          "Vilnius",
+          "Kaunas"
+        ],
+        correct_answer: "Vilnius"
+      },
+      {
+        text: "Quelle est la capitale de la Mongolie",
+        options: [
+          "Oulan-Bator",
+          "Urumqi",
+          "Pekin",
+          "Douchanbé"
+        ],
+        correct_answer: "Oulan-Bator"
+      },
+
+    ]
+  };
+
