a706a813cd284f97481cfe41493d13cc015e46b2
Sebus83 <sebastien.herbiet@gmail.com>
2024-10-30 14:28:29 +0100

    ajout des boutonsavant commit de Charlotte

--- game.js
+++ game.js
@@ -19,14 +19,15 @@ const firstQuestion = quizTableau.questions[0];
 
 quizQuestionHTML.innerText=firstQuestion.text
 
+
 //quizReponseHTML.innerText=firstQuestion.options
 //console.log(quizReponseHTML)
 
 
 // Pour chaque option, créer un bouton et l'ajouter au conteneur
-firstQuestion.options.forEach(option => {
-    const reponseBouton = document.createElement('button');
-    reponseBouton.innerText = option;
-    reponseBouton.classList.add('button');
-    quizReponseHTML.appendChild(reponseBouton);
+firstQuestion.options.forEach(option => { //pour chaque reponse de la liste reponse
+    const reponseBouton = document.createElement('button'); // on crée un bouton reponse
+    reponseBouton.innerText = option; // on récupère le texte de la reponse pour l'intégrer le texte sur le bouton
+    reponseBouton.classList.add('boutonReponse'); // ajout de la classe button à chaque bouton
+    quizReponseHTML.appendChild(reponseBouton); // terminer la boucle lorsqu'il n'y a plus de reponse
   });
