f3f754356634fdcdf3a0018f7edbf6f1ab2e1899
CharlotteBCK <charlotte44300@gmail.com>
2024-10-30 12:27:06 +0100

    DOM question/reponse dans game.js

--- game.js
+++ game.js
@@ -1,22 +1,31 @@
 import  {quizTableau}  from './questions.js'; // Import des questions
 
 // Récupérer les emplacements pour injecter la question et les options
-const quizQuestion = document.getElementById('questions');
-const quizReponse = document.getElementById('reponses');
-console.log(quizQuestion);
-console.log(quizReponse);
+let quizQuestionHTML = document.getElementById('questions');
+
+let quizReponseHTML = document.getElementById('reponses');
+
+console.log(quizQuestionHTML);
+console.log(quizReponseHTML)
 
 // Récupérer la première question
-const firstQuestion = text.questions[0];
+
+let firstQuestion = quizTableau.questions[0];
+
 console.log(firstQuestion);
 
 // Injecter le texte de la question dans l'emplacement dédié
-quizQuestion.innerText = firstQuestion;
 
-// Pour chaque option, créer un bouton et l'ajouter au conteneur
-firstQuestion.__________.forEach(__________ => {
-    const __________ = document.createElement('button');
-    __________.innerText = __________;
-    __________.classList.add('__________');
-    __________.appendChild(__________);
-  });
+
+quizQuestionHTML.innerText=firstQuestion.text
+
+quizReponseHTML.innerText=firstQuestion.options
+
+
+// // Pour chaque option, créer un bouton et l'ajouter au conteneur
+// firstQuestion.__________.forEach(__________ => {
+//     const __________ = document.createElement('button');
+//     __________.innerText = __________;
+//     __________.classList.add('__________');
+//     __________.appendChild(__________);
+//   });
