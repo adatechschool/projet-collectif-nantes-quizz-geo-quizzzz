053d2a7927474507ecea45fc8064d20d38fb7ff0
Sebus83 <sebastien.herbiet@gmail.com>
2024-10-30 15:17:23 +0100

    travail sur le bouton suivant

--- game.js
+++ game.js
@@ -5,14 +5,9 @@ let quizQuestionHTML = document.getElementById('questions');
 
 let quizReponseHTML = document.getElementById('reponses');
 
-// console.log(quizQuestionHTML);
-// console.log(quizReponseHTML);
-
 // Récupérer la première question
 
 const firstQuestion = quizTableau.questions[0];
-
-//console.log(firstQuestion);
 
 // Injecter le texte de la question dans l'emplacement dédié
 
@@ -21,7 +16,6 @@ quizQuestionHTML.innerText=firstQuestion.text
 
 
 //quizReponseHTML.innerText=firstQuestion.options
-//console.log(quizReponseHTML)
 
 
 // Pour chaque option, créer un bouton et l'ajouter au conteneur
@@ -31,3 +25,24 @@ firstQuestion.options.forEach(option => { //pour chaque reponse de la liste repo
     reponseBouton.classList.add('boutonReponse'); // ajout de la classe button à chaque bouton
     quizReponseHTML.appendChild(reponseBouton); // terminer la boucle lorsqu'il n'y a plus de reponse
   });
+
+let currentQuestionIndex = 0 //Déclarer une variable pour suivre la question actuelle (currentQuestionIndex). Au début, elle est égale à zéro. 
+let boutonHTML = document.getElementById('valider-suivant')  
+function loadQuestion() {
+    // Vider le conteneur des options
+    __________.innerHTML = '';
+  
+    // Récupérer la question actuelle
+    const __________ = __________.questions[__________];
+  
+    // Injecter la question dans le HTML
+    __________.innerText = __________;
+  
+    // Injecter les options dans le HTML 
+    __________.__________.forEach(__________ => {
+      const __________ = document.createElement('button');
+      __________.innerText = __________;
+      __________.classList.add('__________');
+      __________.appendChild(__________);
+    });
+  }
